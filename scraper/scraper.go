ad190e5ac8e6494a0b40371e82ffb123b35272ac
kerolos <kerolos.sss@gmail.com>
2021-09-26 18:42:00 +0200

    getting document type

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -3,7 +3,7 @@ package scraper
 import (
 	"fmt"
 	"log"
-	"strconv"
+	"regexp"
 	"strings"
 	"sync"
 
@@ -15,14 +15,30 @@ import (
 
 type Accessable struct {
 	count      int
+	internal   bool
 	accessable bool
+}
+
+type Counts struct {
+	htmlVersion string
+	title       string
+	h1          int
+	h2          int
+	h3          int
+	h4          int
+	h5          int
+	h6          int
+	internal    int
+	external    int
+	inacessable int
+	hasLogin    bool
 }
 
 /**
 * Testing document
 * returns (<a> tags Href, h# tags count)
  */
-func Scrape(url string) (map[string]int, map[string]int) {
+func Scrape(pageURL string) (map[string]int, map[string]int, string) {
 
 	// I chose to count using a dict with UUID to avoid count issues with concurrency
 	// I thought of regular int inside the dict but I don't know how it would behave in racing
@@ -30,10 +46,37 @@ func Scrape(url string) (map[string]int, map[string]int) {
 
 	mainCollector := colly.NewCollector()
 	aCollections := make(map[string]bool)
+
 	separatorUUID := uuid.NewString()
-	mainCollector.OnHTML("body", func(bodyElement *colly.HTMLElement) {
-		// fmt.Println("got a body")
+	doctype := "1"
+	re := regexp.MustCompile(`<[!]{0,1}DOCTYPE.*>`)
+	mainCollector.OnScraped(func(r *colly.Response) {
+		fmt.Println("got a response")
+		fmt.Println(string(r.Body[:100]))
+		found := re.Find(r.Body)
+		if len(found) > 0 {
+			doctype = string(found)
+		}
+
+		fmt.Println(doctype)
+		fmt.Println(doctype)
+		fmt.Println(doctype)
+		fmt.Println(doctype)
+
 	})
+	// mainCollector.OnHTML("body", func(bodyElement *colly.HTMLElement) {
+	// 	fmt.Println("got a body")
+	// 	for i, node := range bodyElement.DOM.Parent().Nodes {
+	// 		fmt.Println("got a DOCTYPE : index: " + strconv.Itoa(i) + " type: ")
+	// 		fmt.Println(node.Data)
+	// 		fmt.Println(node.Attr)
+	// 		fmt.Println(node.Namespace)
+	// 		fmt.Println(*node.Parent)
+	// 		fmt.Println("####")
+	// 	}
+	// })
+	// mainCollector.OnHTML("!DOCTYPE", func(element *colly.HTMLElement) {
+	// })
 
 	mainCollector.OnHTML("a", func(aElement *colly.HTMLElement) {
 		uuidStr := uuid.NewString()
@@ -50,9 +93,9 @@ func Scrape(url string) (map[string]int, map[string]int) {
 		})
 	}
 
-	fmt.Println("starting to collect from: ")
-	fmt.Println(url)
-	mainCollector.Visit(url)
+	// fmt.Println("starting to collect from: ")
+	// fmt.Println(pageURL)
+	mainCollector.Visit(pageURL)
 	// fmt.Println(aCollections)
 	tagsCount := make(map[string]int)
 	for tag, dict := range tagsToCountDicts {
@@ -64,7 +107,7 @@ func Scrape(url string) (map[string]int, map[string]int) {
 		urls[url] += 1
 	}
 
-	return urls, tagsCount
+	return urls, tagsCount, doctype
 }
 
 func DetectIsAccessible(url string) bool {
@@ -80,7 +123,7 @@ func DetectIsAccessible(url string) bool {
 	return err == nil
 }
 
-func GetPageDetails(pageUrl string) (map[string]Accessable, map[string]int) {
+func GetPageDetails(pageUrl string) (map[string]Accessable, map[string]int, string) {
 
 	// parse only base url
 	base, err := url.Parse(pageUrl)
@@ -88,7 +131,7 @@ func GetPageDetails(pageUrl string) (map[string]Accessable, map[string]int) {
 		log.Fatal(err)
 	}
 
-	urls, tagsCount := Scrape(pageUrl)
+	urls, tagsCount, doctype := Scrape(pageUrl)
 	processedURLs := make(map[string]int)
 
 	for key, count := range urls {
@@ -97,6 +140,7 @@ func GetPageDetails(pageUrl string) (map[string]Accessable, map[string]int) {
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		processedURLs[u.String()] += count
 	}
 
@@ -110,15 +154,23 @@ func GetPageDetails(pageUrl string) (map[string]Accessable, map[string]int) {
 		// fmt.Println("worker for: " + key)
 		go func(key string, count int) {
 
-			fmt.Println("worker for: " + strconv.Itoa(count) + "   key :" + key)
+			// fmt.Println("worker for: " + strconv.Itoa(count) + "   key :" + key)
+			u, _ := url.Parse(key)
 			accessable[key] = Accessable{
 				count:      count,
 				accessable: DetectIsAccessible(key),
+				internal:   u.Host == base.Host,
 			}
 			group.Done()
 		}(key, count)
 	}
 
 	group.Wait()
-	return accessable, tagsCount
+
+	return accessable, tagsCount, doctype
 }
+
+// func GetPageDetails(pageUrl string) Counts {
+// 	accessable, tagsCount = GetPageDetails(pageUrl)
+// 	return Counts{}
+// }
--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -10,7 +10,7 @@ func sestScraper(t *testing.T) {
 	// want := "Hello, world."
 	fmt.Println("starting testing scraper")
 
-	urls, headings := Scrape("https://youtube.com")
+	urls, headings, doctype := Scrape("https://youtube.com")
 	if urls == nil || headings == nil {
 		t.Errorf("Scrape = %q, want %q", urls, "not nil")
 	}
@@ -18,6 +18,7 @@ func sestScraper(t *testing.T) {
 	log.Println("Data")
 	log.Println(urls)
 	log.Println(headings)
+	log.Println(doctype)
 
 }
 
@@ -38,30 +39,34 @@ func sestIsAccessible(t *testing.T) {
 
 func TestGetPageDetails(t *testing.T) {
 	url1 := "https://google.com/dsgdfgds"
-	accessible, tags := GetPageDetails(url1)
+	accessible, tags, doctype := GetPageDetails(url1)
 	if len(accessible) > 0 || tags["h1"]+tags["h2"]+tags["h3"]+tags["h4"]+tags["h5"]+tags["h6"] > 0 {
 		t.Errorf("wrong accessability detection url: %q , should not be accessable", url1)
 		fmt.Println("NON ACCESSIBLE")
 		fmt.Println(accessible)
 		fmt.Println(tags)
+		fmt.Println(doctype)
 	} else {
 		fmt.Println("NON ACCESSIBLE")
 		fmt.Println(accessible)
 		fmt.Println(tags)
+		fmt.Println(doctype)
 	}
 
 	url2 := "https://accounts.google.com/AccountChooser?continue=https%3A%2F%2Fdocs.google.com%2Fforms%2Fcreate%3Fusp%3Dforms_alc&followup=https%3A%2F%2Fdocs.google.com%2Fforms%2Fcreate%3Fusp%3Dforms_alc&service=wise&ltmpl=forms"
-	accessible2, tags2 := GetPageDetails(url2)
+	accessible2, tags2, doctype2 := GetPageDetails(url2)
 	if len(accessible2) > 0 || len(tags2) > 0 {
 		fmt.Println("ACCESSIBLE")
 		fmt.Println(accessible2)
 		fmt.Println(tags2)
+		fmt.Println(doctype2)
 
 	} else {
 		t.Errorf("wrong accessability detection url: %q , should not be accessable", url1)
 		fmt.Println("ACCESSIBLE")
 		fmt.Println(accessible2)
 		fmt.Println(tags2)
+		fmt.Println(doctype2)
 	}
 
 }
