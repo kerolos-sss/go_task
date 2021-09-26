e4d530f35ad6b658038ef48bd044f4bb70da7367
kerolos <kerolos.sss@gmail.com>
2021-09-26 15:20:15 +0200

    making the url detection concurrent

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -2,31 +2,49 @@ package scraper
 
 import (
 	"fmt"
+	"log"
+	"strings"
+	"sync"
+
+	"net/url"
 
 	"github.com/gocolly/colly/v2"
 	"github.com/google/uuid"
 )
 
-func Scrape(url string) ([]string, map[string]int) {
+type Accessable struct {
+	count      int
+	accessable bool
+}
 
-	mainCollector := colly.NewCollector()
-	aCollections := make(map[string]bool)
+/**
+* Testing document
+* returns (<a> tags Href, h# tags count)
+ */
+func Scrape(url string) (map[string]int, map[string]int) {
 
-	mainCollector.OnHTML("body", func(bodyElement *colly.HTMLElement) {
-		fmt.Println("got a body")
-	})
-	mainCollector.OnHTML("a", func(aElement *colly.HTMLElement) {
-		aCollections[aElement.Attr("href")] = true
-		fmt.Println(aCollections)
-	})
-	tagsToCount := []string{"h1", "h2", "h3", "h4", "h5", "h6"}
 	// I chose to count using a dict with UUID to avoid count issues with concurrency
 	// I thought of regular int inside the dict but I don't know how it would behave in racing
 	// I thought of slices they did not seem a good choice at all
+
+	mainCollector := colly.NewCollector()
+	aCollections := make(map[string]bool)
+	separatorUUID := uuid.NewString()
+	mainCollector.OnHTML("body", func(bodyElement *colly.HTMLElement) {
+		// fmt.Println("got a body")
+	})
+
+	mainCollector.OnHTML("a", func(aElement *colly.HTMLElement) {
+		uuidStr := uuid.NewString()
+		aCollections[uuidStr+separatorUUID+aElement.Attr("href")] = true
+		// fmt.Println(aCollections)
+	})
+	tagsToCount := []string{"h1", "h2", "h3", "h4", "h5", "h6"}
+
 	tagsToCountDicts := make(map[string]map[string]bool)
 	for _, tag := range tagsToCount {
 		tagsToCountDicts[tag] = make(map[string]bool)
-		mainCollector.OnHTML("a", func(aElement *colly.HTMLElement) {
+		mainCollector.OnHTML(tag, func(aElement *colly.HTMLElement) {
 			tagsToCountDicts[tag][uuid.NewString()] = true
 		})
 	}
@@ -39,15 +57,13 @@ func Scrape(url string) ([]string, map[string]int) {
 	for tag, dict := range tagsToCountDicts {
 		tagsCount[tag] = len(dict)
 	}
-	urls := make([]string, len(aCollections))
-	index := 0
+	urls := make(map[string]int)
 	for key, _ := range aCollections {
-		urls[index] = key
-		index++
+		url := strings.Split(key, separatorUUID)[1]
+		urls[url] += 1
 	}
 
 	return urls, tagsCount
-
 }
 
 func DetectIsAccessible(url string) bool {
@@ -61,4 +77,44 @@ func DetectIsAccessible(url string) bool {
 
 	collector.Visit(url)
 	return err == nil
+}
+
+func GetPageDetails(pageUrl string) (map[string]Accessable, map[string]int) {
+
+	// parse only base url
+	base, err := url.Parse(pageUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	urls, tagsCount := Scrape(pageUrl)
+	processedURLs := make(map[string]int)
+
+	for key, count := range urls {
+		// and then use it to parse relative URLs
+		u, err := base.Parse(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		processedURLs[u.String()] += count
+	}
+
+	accessable := make(map[string]Accessable)
+
+	var group sync.WaitGroup
+
+	for key, count := range processedURLs {
+		// and then use it to parse relative URLs
+		group.Add(1)
+		go func() {
+			accessable[key] = Accessable{
+				count:      count,
+				accessable: DetectIsAccessible(key),
+			}
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+	return accessable, tagsCount
 }
--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestScraper(t *testing.T) {
+func sestScraper(t *testing.T) {
 	// want := "Hello, world."
 	fmt.Println("starting testing scraper")
 
@@ -21,7 +21,7 @@ func TestScraper(t *testing.T) {
 
 }
 
-func TestIsAccessible(t *testing.T) {
+func sestIsAccessible(t *testing.T) {
 	url1 := "https://google.com/dsgdfgds"
 	accessible := DetectIsAccessible(url1)
 	if accessible {
@@ -32,6 +32,36 @@ func TestIsAccessible(t *testing.T) {
 	accessible2 := DetectIsAccessible(url2)
 	if accessible2 == false {
 		t.Errorf("wrong accessability detection url: %q , should be accessable", url2)
+	}
+
+}
+
+func TestGetPageDetails(t *testing.T) {
+	url1 := "https://google.com/dsgdfgds"
+	accessible, tags := GetPageDetails(url1)
+	if len(accessible) > 0 || tags["h1"]+tags["h2"]+tags["h3"]+tags["h4"]+tags["h5"]+tags["h6"] > 0 {
+		t.Errorf("wrong accessability detection url: %q , should not be accessable", url1)
+		fmt.Println("NON ACCESSIBLE")
+		fmt.Println(accessible)
+		fmt.Println(tags)
+	} else {
+		fmt.Println("NON ACCESSIBLE")
+		fmt.Println(accessible)
+		fmt.Println(tags)
+	}
+
+	url2 := "https://google.com"
+	accessible2, tags2 := GetPageDetails(url2)
+	if len(accessible2) > 0 || len(tags2) > 0 {
+		fmt.Println("ACCESSIBLE")
+		fmt.Println(accessible2)
+		fmt.Println(tags2)
+
+	} else {
+		t.Errorf("wrong accessability detection url: %q , should not be accessable", url1)
+		fmt.Println("ACCESSIBLE")
+		fmt.Println(accessible2)
+		fmt.Println(tags2)
 	}
 
 }
